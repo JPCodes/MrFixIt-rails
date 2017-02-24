e005bfc72f34fb4b33fa7731fbf1b3eb1e67295a
Jim Padilla <jp_code2013@yahoo.com>
2017-02-24 14:53:21 -0800

    Add user factory, first integration spec for jobs

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,14 @@
 FactoryGirl.define do
+  factory :user do
+    sequence(:email){|n| "user#{n}@factory.com" }
+    password '123456'
+  end
+
   factory :job do
     title '1 Acre lawn needs mowing'
     description 'Have 1 acre lawn that needs mowing asap, extra $$$ if weeds are pulled'
     completed false
     pending false
-    active_job false    
+    active_job false
   end
 end
---
+++ spec/features/create_jobs_pages_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+describe 'Create a Job process' do
+  it 'will add a Job', js: true do
+    user = FactoryGirl.create(:user)
+    login_as(user)
+    visit root_path
+    click_link 'Add Your Job'
+    fill_in 'Title', :with => 'Lawn Mowing needed!'
+    fill_in 'Description', :with => 'Please mow my lawn!'
+    click_on 'Create Job'
+    expect(page).to have_content('Lawn Mowing needed!')
+  end
+end
