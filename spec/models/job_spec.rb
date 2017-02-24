a5dca6fe6e8785e91740d481c4da8e463ada029e
Jim Padilla <jp_code2013@yahoo.com>
2017-02-24 14:20:07 -0800

    Add job unit test for complete job method

--- spec/models/job_spec.rb
+++ spec/models/job_spec.rb
@@ -4,17 +4,27 @@ describe Job do
   it { should validate_presence_of :title }
   it { should validate_presence_of :description }
 
-  it 'Toggles a jobs active state to true when false' do
-    job = FactoryGirl.create(:job)
-    job.toggle_job
-    expect(job.active_job).to(eq(true))
+  describe('Job#toggle_job') do
+    it 'Toggles a jobs active state to true when false' do
+      job = FactoryGirl.create(:job)
+      job.toggle_job
+      expect(job.active_job).to(eq(true))
+    end
+
+    it 'Toggles a jobs active state to false when true' do
+      job = FactoryGirl.create(:job)
+      job.active_job = true
+      job.toggle_job
+      expect(job.active_job).to(eq(false))
+    end
   end
 
-  it 'Toggles a jobs active state to false when true' do
-    job = FactoryGirl.create(:job)
-    job.active_job = true
-    job.toggle_job
-    expect(job.active_job).to(eq(false))
+  describe('Job#complete_job') do
+    it 'Changes a jobs complete state to true' do
+      job = FactoryGirl.create(:job)
+      job.complete_job
+      expect(job.completed).to(eq(true))
+    end
   end
 
 end
