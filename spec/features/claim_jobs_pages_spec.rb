fa265ac3cbcaf637098d4304972f4aef34284619
Jim Padilla <jp_code2013@yahoo.com>
2017-02-24 15:34:04 -0800

    Add integration spec that disallows claiming of a claimed Job as a worker

--- spec/features/claim_jobs_pages_spec.rb
+++ spec/features/claim_jobs_pages_spec.rb
@@ -4,17 +4,23 @@ describe 'Create a Job process' do
   before do
     @user = FactoryGirl.create(:user)
     @worker = FactoryGirl.create(:worker)
-    @job = FactoryGirl.create(:job)
+    @job_unclaimed = FactoryGirl.create(:job)
+    @job_claimed = FactoryGirl.create(:job, pending: true)
   end
 
   it 'will claim a Job as a worker', js: true do
     login_as(@worker)
     visit root_path
     click_link 'See All Jobs'
-    click_link @job.title
+    click_link @job_unclaimed.title
     click_link 'click here to claim it now'
     expect(page).to have_content('You\'ve successfully claimed this job.')
   end
 
+  it 'will disallow claiming of a claimed Job as a worker', js: true do
+    login_as(@worker)
+    visit job_path(@job_claimed)
+    expect(page).to have_content('Someone has already claimed this job.')
+  end
 
 end
